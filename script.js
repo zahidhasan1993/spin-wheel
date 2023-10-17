2be661545e68953e198cf3fe71a8cfb7296809e2
devZahid <officialzahidhasan19932021@gmail.com>
2023-10-17 10:57:16 +0600

    almost everything done in this spin wheels project

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
 const wheel = document.getElementById("wheel");
 const spinBtn = document.getElementById("spin-btn");
 const finalValue = document.getElementById("final-value");
-//Object that stores values of minimum and maximum angle for a value
-const rotationValues = [
-  { minDegree: 0, maxDegree: 30, value: 2 },
-  { minDegree: 31, maxDegree: 90, value: 1 },
-  { minDegree: 91, maxDegree: 150, value: 6 },
-  { minDegree: 151, maxDegree: 210, value: 5 },
-  { minDegree: 211, maxDegree: 270, value: 4 },
-  { minDegree: 271, maxDegree: 330, value: 3 },
-  { minDegree: 331, maxDegree: 360, value: 2 },
+
+// Object that stores values of minimum and maximum angle for a value
+let person1 = "";
+let person2 = "";
+let person3 = "";
+let person4 = "";
+let person5 = "";
+let person6 = "";
+
+const inputElements = [
+  document.getElementById("name1"),
+  document.getElementById("name2"),
+  document.getElementById("name3"),
+  document.getElementById("name4"),
+  document.getElementById("name5"),
+  document.getElementById("name6"),
 ];
+
+inputElements.forEach((input, index) => {
+  input.addEventListener("input", function (event) {
+    switch (index) {
+      case 0:
+        person1 = event.target.value;
+        console.log(person1);
+        break;
+      case 1:
+        person2 = event.target.value;
+        break;
+      case 2:
+        person3 = event.target.value;
+        break;
+      case 3:
+        person4 = event.target.value;
+        break;
+      case 4:
+        person5 = event.target.value;
+        break;
+      case 5:
+        person6 = event.target.value;
+        break;
+    }
+  });
+});
+
+let rotationValues = [];
+
 //Size of each piece
 const data = [16, 16, 16, 16, 16, 16];
 //background color for each piece
@@ -30,7 +66,7 @@ let myChart = new Chart(wheel, {
   type: "pie",
   data: {
     //Labels(values which are to be displayed on chart)
-    labels: [1, 2, 3, 4, 5, 6],
+    labels: ["Slot1", "Slot2", "Slot3", "Slot4", "Slot5", "Slot6"],
     //Settings for dataset/pie
     datasets: [
       {
@@ -63,7 +99,7 @@ const valueGenerator = (angleValue) => {
   for (let i of rotationValues) {
     //if the angleValue is between min and max then display it
     if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
-      finalValue.innerHTML = `<p>Value: ${i.value}</p>`;
+      finalValue.innerHTML = `<p>Winner is: ${i.value}</p>`;
       spinBtn.disabled = false;
       break;
     }
@@ -76,7 +112,17 @@ let count = 0;
 let resultValue = 101;
 //Start spinning
 spinBtn.addEventListener("click", () => {
+   rotationValues = [
+    { minDegree: 0, maxDegree: 30, value: person2 },
+    { minDegree: 31, maxDegree: 90, value: person1 },
+    { minDegree: 91, maxDegree: 150, value: person6 },
+    { minDegree: 151, maxDegree: 210, value: person5 },
+    { minDegree: 211, maxDegree: 270, value: person4 },
+    { minDegree: 271, maxDegree: 330, value: person3 },
+    { minDegree: 331, maxDegree: 360, value: person2 },
+  ];
   spinBtn.disabled = true;
+  console.log(rotationValues);
   //Empty final value
   finalValue.innerHTML = `<p>Good Luck!</p>`;
   //Generate random degrees to stop at
